1097a5075ab22273e674f95bd972e70dcbbeb00e
Damian <damian.ruta@hotmail.com>
2024-02-06 18:20:14 +0100

    Start project

---
+++ src/WasteControl.Core/Entities/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WasteControl.Core.Entities
+{
+    public class User
+    {
+        
+    }
+}
